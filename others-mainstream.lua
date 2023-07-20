d49062178bd6951c9814223550db1da8f44efb18
Bruno-366 <brunoarias366@gmail.com>
2023-07-20 22:57:39 +0200

    fix lua embedding in .md

--- others-mainstream.lua
+++ others-mainstream.lua
@@ -1,61 +1,61 @@
---- [function]
-   function say(text)
-     print(text);
-     return true;
-   end
---- [function]
+--#### [function]
+ function say(text)
+ print(text);
+ return true;
+end
+--### [function]
 
---- [anonFunction]
-   say = function (text) print(text); return true; end
---- [anonFunction]
+--### [anonFunction]
+say = function (text) print(text); return true; end
+--### [anonFunction]
 
---- [ifElse]
-   if true then
-      say("it is true");
-   else
-      say("it is not true");
-   end
---- [ifElse]
+--### [ifElse]
+if true then
+  say("it is true");
+else
+  say("it is not true");
+end
+--### [ifElse]
 
---- [loops]
-   for i = 1,5 do
-      say(i);
-   end
+--### [loops]
+for i = 1,5 do
+  say(i);
+end
 
-   -- execute once, stop when true
-   repeat
-     say("this is executed once");
-   until true
+-- execute once, stop when true
+repeat
+ say("this is executed once");
+until true
 
-   -- execute only if true
-   while true do
-      say("this is an infinite loop");
-   end
---- [loops]
+-- execute only if true
+while true do
+  say("this is an infinite loop");
+end
+--### [loops]
 
---- [short]
-   x = true and say("it is true") or say("it is not true")
-   -- the `x =` is required since lua distinguishes between statements and expressions
---- [short]
+--### [short]
+x = true and say("it is true") or say("it is not true")
+-- the `x =` is required since lua distinguishes between statements and expressions
+--### [short]
 
---- [recursion]
-   function while_loop ()
-     say("yes");
-     return true and while_loop();
-   end
+--### [recursion]
+function while_loop ()
+ say("yes");
+ return true and while_loop();
+end
 
-   function for_loop (i)
-     i = i+1;
-     say(i);
-     return (i == 5) or for_loop(i);
-   end
+function for_loop (i)
+ i = i+1;
+ say(i);
+ return (i == 5) or for_loop(i);
+end
 
-   for_loop(0)   -- <1>
-   while_loop()  -- <2>
-   -- (1) execute for-loop first
-   -- (2) since the while-loop will enter an infinite loop,
-   --     you will need to cancel the execution by pressing CTRL-C
---- [recursion]
+for_loop(0)   -- <1>
+while_loop()  -- <2>
+-- (1) execute for-loop first
+-- (2) since the while-loop will enter an infinite loop,
+--     you will need to cancel the execution by pressing CTRL-C
+--### [recursion]
 
 -- == Functional constructs
 
